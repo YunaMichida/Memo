60c1fcfa84c87c4f634027260ab8bf804c20014c
Yuna Michida <yuna.iseki@gmail.com>
2020-10-09 00:38:52 +0900

    初回コミット

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+    require_once './public_html/db_connect.php';
+
+    $message = 'Hello World';
+    require_once './public_html/views/index.tpl.php';
---
+++ show.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once 'public_html/db_connect.php';
+
+    if(isset($_REQUEST['id'])) {
+        $id = $_REQUEST['id'];
+        $message = 'Show note #' . $id;
+        $note = Note::find($id);
+    }
+    $my_html = Michelf\Markdown::defaultTransform($note->content);
+
+    require_once 'public_html/views/show.tpl.php';
+    
