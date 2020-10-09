43ab2f59417827d81d395e22c99770841fc46b88
Yuna Michida <yuna.iseki@gmail.com>
2020-10-09 22:39:05 +0900

    新規メモリンク作成

--- index.php
+++ index.php
@@ -3,4 +3,6 @@
     require_once './public_html/db_connect.php';
 
     $message = 'Hello World';
+    $notes = Note::all();
+    
     require_once './public_html/views/index.tpl.php';
--- show.php
+++ show.php
@@ -7,7 +7,7 @@
         $message = 'Show note #' . $id;
         $note = Note::find($id);
     }
+
     $my_html = Michelf\Markdown::defaultTransform($note->content);
 
     require_once 'public_html/views/show.tpl.php';
-    
