6a09af3dc600d6c94997a16ee18e48d6f4ffe6d1
Yuna Michida <yuna.iseki@gmail.com>
2020-10-11 21:52:09 +0900

    new.tpl.phpを呼び出すコードに書き換え

--- public_html/new.php
+++ public_html/new.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang='ja'>
-    <?php //include('header.inc.php'); ?>
-    <body>
+<?php
 
-        <h1><?= $message ?></h1>
-
-        <form action='create.php' method='post'>
-            <label for='title'>タイトル</label><br>
-            <input type='text' name='title' >
-            <p></p>
-            <label for='content'>本文</label><br>
-            <textarea name='content' cols='40' rows='10'></textarea>
-            <p></p>
-            <button type='submit'>作成する</button>
-        </form>
-
-        <p><a href='index.php'>一覧に戻る</a></p>
-
-        <?php //include('footer.inc.php'); ?>
-    </body>
-</html>
+    $message = 'New note';
+    require_once 'views/new.tpl.php';
