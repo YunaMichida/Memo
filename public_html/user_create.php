f394d754ee1aa100c032cbbffe2a52a40d2395f4
道田 悠聖 <50403029+Michida-Yusei@users.noreply.github.com>
2020-10-18 19:24:48 +0900

    Update user_create.php

--- public_html/user_create.php
+++ public_html/user_create.php
@@ -1,19 +1,20 @@
 <?php
 require_once 'db_connect.php';
 
-$user = new User;
-$user->username = $_POST['username'];
-$user->password = $_POST['password'];
-$user->save();
-exit;
-
 try{
-    throw new Exception
-    $user->username = $_POST['username'];
-    } catch (Exception $e) {
-        echo 'エラー:' . $e->getMessage();
+    $userName = $_POST['username'];
+    $password = $_POST['password'];
+    
+    if( is_null($userName) || is_null($password) ) {
+        throw new Exception('ユーザネームかパスワードが空です。');
     }
 
+    $user = new User;
+    $user->username = $userName;
+    $user->password = $password;
+    $user->save();
 
-
-
+} catch (Exception $e) {
+    $errorMessage = $e->getMessage();
+    require './view/signup.tpl.php';
+}
