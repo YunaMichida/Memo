3c2cca8152d3081583d53363b77839737a17164a
道田 <michida@mediaxis.jp>
2021-01-23 11:22:23 +0900

    インデント調整

--- public_html/views/signup.tpl.php
+++ public_html/views/signup.tpl.php
@@ -3,16 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 	<title>新規登録画面</title>
 </head>
 <body>
-
-	<h1>新規登録画面</h1>
-		<form action="user_create.php" method="POST">
-			UserName<input type="text" name="username" value=""><br>
-			Password<input type="password" name="password" value=""><br>
-			<input type="submit" name="signup" value="新規登録"></a>
-		</form>
+    <h1>新規登録画面</h1>
+    <form action="user_create.php" method="POST">
+        UserName<input type="text" name="username" value=""><br>
+        Password<input type="password" name="password" value=""><br>
+        <input type="submit" name="signup" value="新規登録"></a>
+    </form>
 </body>
 </html>
